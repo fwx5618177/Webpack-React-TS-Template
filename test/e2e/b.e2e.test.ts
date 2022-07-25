309ad5d9c41eb82a6730e2ad6c7c53e88ef244c8
Wenxuan Feng <279357596@qq.com>
2022-07-25 13:18:05 +0800

    feat: update

---
+++ test/e2e/b.e2e.test.ts
@@ -0,0 +1,20 @@
+import puppeteer from 'puppeteer'
+
+describe('Login', () => {
+    it('should login with failure', async () => {
+        const browser = await puppeteer.launch({
+            headless: false,
+            defaultViewport: { width: 1920, height: 1080 },
+            args: ['--start-maximized'],
+        })
+        const page: puppeteer.Page = await browser.newPage()
+        await page.goto(`${URL}`)
+        for (let i = 0; i < 100; i++) {
+            await page.waitForSelector('.logo')
+            await page.click('.logo')
+        }
+
+        await page.close()
+        browser.close()
+    })
+})
